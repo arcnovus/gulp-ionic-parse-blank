8ff6c38de0c1983722c8a7625b1f4624dbb7d7a5
Nabil Sourour <bill.sourour@arcnovus.com>
2015-12-11 14:27:16 -0500

    fix read of constants from environment variables

--- gulp/constants.js
+++ gulp/constants.js
@@ -6,15 +6,16 @@ var conf = require('./conf');
 
 var $ = require('gulp-load-plugins')();
 gulp.task('constants', function () {
-    var configPath = 'app.constants.dev.json';
+    var configPath = 'app.constants.dev.json',
+        constants = {};
+
     if (process.env.NODE_ENV === 'prod') {
         configPath = 'app.constants.prod.json';
+        constants.PARSE_APP_ID = process.ENV.PARSE_APP_ID;
+        constants.PARSE_API_KEY = process.ENV.PARSE_API_KEY;
         return gulp.src(configPath)
             .pipe($.ngConstant({
-                constants: {
-                    PARSE_APP_ID: process.ENV.PARSE_APP_ID,
-                    PARSE_API_KEY: process.ENV.PARSE_API_KEY
-                }
+                constants: constants
             }))
             .pipe(gulp.dest('src/app/'));
     }
